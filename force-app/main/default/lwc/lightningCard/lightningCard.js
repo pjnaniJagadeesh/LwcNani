5bce06d63085f9122eebbc477093196413d7b775
Jagadeesh <Pjnani76@gmail.com>
2025-08-01 20:27:21 +0530

    Messages

---
+++ force-app/main/default/lwc/lightningCard/lightningCard.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class LightningCard extends LightningElement {
+
+    handleClick() {
+        // Handle button click event
+        console.log('Button clicked!');
+    }
+
+    // Additional methods or properties can be added here
+}
